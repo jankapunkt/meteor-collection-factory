5f8a01dfacbbc75f59c53867ba0b2366977a7899
jankapunkt <jkuester@uni-bremen.de>
2017-06-27 14:23:12 +0200

    initial package state

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'jkuester:meteor-collection-factory',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.5');
+  api.use('ecmascript');
+  api.mainModule('meteor-collection-factory.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('jkuester:meteor-collection-factory');
+  api.mainModule('meteor-collection-factory-tests.js');
+});
