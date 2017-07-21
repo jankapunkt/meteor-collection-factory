8fee7d6487aa9bc4fa6762c8bbb6eb7647e953e2
jankapunkt <jkuester@uni-bremen.de>
2017-07-21 11:59:13 +0200

    remove git deps and use only atmosphere deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'jkuester:meteor-collection-factory',
-	version: '0.1.2',
+	version: '0.1.3',
 	// Brief, one-line summary of the package.
 	summary: 'Factory for creating Mongo.Collection instances.',
 	// URL to the Git repository containing the source code for this package.
