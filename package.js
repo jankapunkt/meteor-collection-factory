a39450d9a0d99a75e31d8c77d7ea70e60afd2f96
Jan Küster <jkuester@uni-bremen.de>
2018-11-02 18:05:10 +0100

    removed unused deps

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'jkuester:meteor-collection-factory',
-  version: '0.1.6',
+  version: '0.1.7',
   // Brief, one-line summary of the package.
   summary: 'Factory for creating Mongo.Collection instances.',
   // URL to the Git repository containing the source code for this package.
@@ -16,10 +16,6 @@ Package.onUse(function (api) {
   api.versionsFrom('1.5')
   api.use('ecmascript')
   api.use('mongo')
-  api.use('dburles:collection-helpers@1.1.0')
-  api.use('aldeed:collection2-core@2.0.1')
-  api.use('tmeasday:check-npm-versions@0.3.1')
   api.use('dburles:mongo-collection-instances@0.3.5')
   api.mainModule('meteor-collection-factory.js')
 })
-
